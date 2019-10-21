164f54fa40cbfc738d998622aa78a5f063db7f78
ngoctienhuynh1999 <ngoctienhuynh1999@gmail.com>
2019-10-21 23:45:38 +0700

    hotel room 1

---
+++ js/Hotel.js
@@ -0,0 +1,48 @@
+var slides = document.querySelectorAll('.slideshow-container .mySlides');
+var currentSlide = 0;
+var slideInterval;
+
+function nextSlide() {
+  if (currentSlide > slides.length - 1) {
+    currentSlide = 1
+  }
+  slides[currentSlide].className = 'mySlides';
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = 'mySlides showing';
+}
+
+function prevSlide() {
+  slides[currentSlide].className = 'mySlides';
+  if (currentSlide < 1) {
+    currentSlide = slides.length - 1
+  } else {
+    currentSlide = (currentSlide - 1) % slides.length;
+  }
+
+  slides[currentSlide].className = 'mySlides showing';
+}
+
+var playing = false;
+
+function pauseSlideshow() {
+  playing = false;
+  clearInterval(slideInterval);
+}
+
+$(document).ready(function playSlideshow() {
+  playing = true;
+  slideInterval = setInterval(nextSlide, 2000);
+});
+
+function playSlideshow() {
+  playing = true;
+  slideInterval = setInterval(nextSlide, 2000);
+}
+
+function auto() {
+  if (playing) {
+    pauseSlideshow();
+  } else {
+    playSlideshow();
+  }
+};
